252239278decaebdd8ec127a9be2993b12844af7
Dev Kumar <devkr074@gmail.com>
2025-04-08 23:28:22 +0530

    Updated Statistic Component

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -3,6 +3,8 @@ import style from "./Statistic.module.css"
 function Statistic() {
     const [mostRuns, setMostRuns] = useState([]);
     const [mostFifties, setMostFifties] = useState([]);
+    const [mostCentury, setMostCentury] = useState([]);
+    const [mostSixes, setMostSixes] = useState([]);
     const [mostWickets, setMostWickets] = useState([]);
     const [player, setPlayer] = useState([]);
     const [tab, setTab] = useState("Most Runs");
@@ -14,14 +16,17 @@ function Statistic() {
         setTotalMatchPlayed(totalMatchPlayed);
         setPlayer(player);
         if (totalMatchPlayed) {
-            statistic.sort((a, b) => a.battingStatistic.runs == b.battingStatistic.runs ? a.battingStatistic.innings - b.battingStatistic.innings : b.battingStatistic.runs - a.battingStatistic.runs);
-            setMostRuns(statistic.slice(0, Math.min(statistic.filter((playerData) => (playerData.battingStatistic.innings > 0)).length, 10)));
-            statistic.sort((a, b) => a.battingStatistic.halfCentury == b.battingStatistic.halfCentury ? b.battingStatistic.runs - a.battingStatistic.runs : b.battingStatistic.halfCentury - a.battingStatistic.halfCentury);
-            setMostFifties(statistic.slice(0, Math.min(statistic.filter((playerData) => (playerData.battingStatistic.halfCentury > 0)).length, 10)));
-            statistic.sort((a, b) => a.bowlingStatistic.wickets == b.bowlingStatistic.wickets ? a.bowlingStatistic.runs - b.bowlingStatistic.runs : b.bowlingStatistic.wickets - a.bowlingStatistic.wickets);
-            setMostWickets(statistic.slice(0, Math.min(statistic.filter((playerData) => (playerData.bowlingStatistic.balls > 0)).length, 10)));
+            const mostRuns = statistic.filter((playerData) => (playerData.battingStatistic.innings > 0)).sort((a, b) => a.battingStatistic.runs == b.battingStatistic.runs ? a.battingStatistic.innings - b.battingStatistic.innings : b.battingStatistic.runs - a.battingStatistic.runs).slice(0, Math.min(statistic.length, 10));
+            setMostRuns(mostRuns);
+            const mostFifties = statistic.filter((playerData) => (playerData.battingStatistic.halfCentury > 0)).sort((a, b) => a.battingStatistic.halfCentury == b.battingStatistic.halfCentury ? b.battingStatistic.runs - a.battingStatistic.runs : b.battingStatistic.halfCentury - a.battingStatistic.halfCentury).slice(0, Math.min(statistic.length, 10));
+            setMostFifties(mostFifties);
+            const mostCentury = statistic.filter((playerData) => (playerData.battingStatistic.century > 0)).sort((a, b) => a.battingStatistic.century == b.battingStatistic.century ? b.battingStatistic.runs - a.battingStatistic.runs : b.battingStatistic.century - a.battingStatistic.century).slice(0, Math.min(statistic.length, 10));
+            setMostCentury(mostCentury);
+            const mostSixes = statistic.filter((playerData) => (playerData.battingStatistic.sixes > 0)).sort((a, b) => a.battingStatistic.sixes == b.battingStatistic.sixes ? b.battingStatistic.runs - a.battingStatistic.runs : b.battingStatistic.sixes - a.battingStatistic.sixes).slice(0, Math.min(statistic.length, 10));
+            setMostSixes(mostSixes);
+            const mostWickets = statistic.filter((playerData) => (playerData.bowlingStatistic.wickets > 0)).sort((a, b) => a.bowlingStatistic.wickets == b.bowlingStatistic.wickets ? a.bowlingStatistic.runs - b.bowlingStatistic.runs : b.bowlingStatistic.wickets - a.bowlingStatistic.wickets).slice(0, Math.min(statistic.length, 10));
+            setMostWickets(mostWickets);
         }
-        console.log(statistic);
     }, []);
     function handleTabChange(e) {
         setTab(e.target.value);
@@ -34,11 +39,13 @@ function Statistic() {
                     <select onChange={handleTabChange} value={tab}>
                         <option value="Most Runs">Most Runs</option>
                         <option value="Most Fifties">Most Fifties</option>
+                        <option value="Most Century">Most Century</option>
+                        <option value="Most Sixes">Most Sixes</option>
                         <option value="Most Wickets">Most Wickets</option>
                     </select>
                 </div>
                 <div className={style.containerContent}>
-                    {tab == "Most Runs" && (totalMatchPlayed ? (
+                    {tab == "Most Runs" && ((totalMatchPlayed && mostRuns.length > 0) ? (
                         <div className={style.section}>
                             <table>
                                 <thead>
@@ -64,7 +71,7 @@ function Statistic() {
                             </table>
                         </div>
                     ) : <p>No Data Available Currently!</p>)}
-                    {tab == "Most Fifties" && (totalMatchPlayed ? (
+                    {tab == "Most Fifties" && ((totalMatchPlayed && mostFifties.length > 0) ? (
                         <div className={style.section}>
                             <table>
                                 <thead>
@@ -90,7 +97,59 @@ function Statistic() {
                             </table>
                         </div>
                     ) : <p>No Data Available Currently!</p>)}
-                    {tab == "Most Wickets" && (totalMatchPlayed ? (
+                    {tab == "Most Sixes" && ((totalMatchPlayed && mostSixes.length > 0) ? (
+                        <div className={style.section}>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Batter</th>
+                                        <th>M</th>
+                                        <th>I</th>
+                                        <th>R</th>
+                                        <th>6s</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {mostSixes.map((playerData, index) =>
+                                        <tr key={playerData.playerId}>
+                                            <td><span>{index + 1}</span><span>{player[playerData.playerId - 1].playerName}</span></td>
+                                            <td>{playerData.battingStatistic.matches}</td>
+                                            <td>{playerData.battingStatistic.innings}</td>
+                                            <td>{playerData.battingStatistic.runs}</td>
+                                            <td>{playerData.battingStatistic.sixes}</td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    ) : <p>No Data Available Currently!</p>)}
+                    {tab == "Most Century" && ((totalMatchPlayed && mostCentury.length > 0) ? (
+                        <div className={style.section}>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Batter</th>
+                                        <th>M</th>
+                                        <th>I</th>
+                                        <th>R</th>
+                                        <th>100s</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {mostCentury.map((playerData, index) =>
+                                        <tr key={playerData.playerId}>
+                                            <td><span>{index + 1}</span><span>{player[playerData.playerId - 1].playerName}</span></td>
+                                            <td>{playerData.battingStatistic.matches}</td>
+                                            <td>{playerData.battingStatistic.innings}</td>
+                                            <td>{playerData.battingStatistic.runs}</td>
+                                            <td>{playerData.battingStatistic.century}</td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    ) : <p>No Data Available Currently!</p>)}
+                    {tab == "Most Wickets" && ((totalMatchPlayed && mostWickets.length > 0) ? (
                         <div className={style.section}>
                             <table>
                                 <thead>
