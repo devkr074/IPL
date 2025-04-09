6f066e9680f7bd8cf4a29f91733657dbc802868c
Dev Kumar <devkr074@gmail.com>
2025-04-09 18:31:09 +0530

    Formatted Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import Team from "./components/Team/Team.jsx";
