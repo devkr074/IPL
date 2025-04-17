a92356a79a75c8812f918b954af76cc9d41687c2
Dev Kumar <devkr074@gmail.com>
2025-04-17 20:28:51 +0530

    Updated Fixture Component

--- src/components/Fixture/Fixture.jsx
+++ src/components/Fixture/Fixture.jsx
@@ -4,34 +4,50 @@ import style from './Fixture.module.css'
 function Fixture() {
     const [fixture, setFixture] = useState([]);
     const [teams, setTeams] = useState([]);
+    const [userTeamId, setUserTeamId] = useState(null);
+    const [leagueMatches, setLeagueMatches] = useState(null);
     const navigate = useNavigate();
     useEffect(() => {
+        document.title = "IPL - Fixture";
         const fixture = JSON.parse(localStorage.getItem("fixture"));
         const teams = JSON.parse(localStorage.getItem("teams"));
+        const userTeamId = Number(localStorage.getItem("userTeamId"));
+        let leagueMatches = [];
+        for (let i = 0; i < 70; i++) {
+            leagueMatches.push(fixture[i]);
+        }
         setFixture(fixture);
+        console.log(leagueMatches);
         setTeams(teams);
+        setUserTeamId(userTeamId);
+        setLeagueMatches(leagueMatches);
     }, []);
     function handleMatch(matchId) {
         if (fixture[matchId - 1].matchStatus == 'completed') {
             navigate(`/match/${matchId}`);
         }
     }
+    function isUserMatch(match) {
+        return ((match.homeTeamId == userTeamId) || (match.awayTeamId == userTeamId));
+    }
     return (
         <>
             <div className={style.container}>
-                <h2 className={style.title}>Fixture List</h2>
-                <div className={style.fixtureGrid}>
-                    {fixture.map((match, index) => (
-                        <div key={index} className={style.matchCard} onClick={() => handleMatch(match.matchId)}>
-                            {(match.homeTeamId != null ?
-                                <img src={teams[match.homeTeamId - 1]?.logo} alt="" height={100} /> :
-                                <p>TBC</p>)}
-                            <span className={style.vs}>V/S</span>
-                            {(match.homeTeamId != null ?
-                                <img src={teams[match.awayTeamId - 1]?.logo} alt="" height={100} /> :
-                                <p>TBC</p>)}
-                        </div>
-                    ))}
+                <div className={style.containerHeader}>
+                    <p>IPL - Fixture</p>
+                </div>
+                <div className={style.containerContent}>
+                    <div className={style.tabsContainer}>
+                        <button>League Matches</button>
+                        <button>Play-off Matches</button>
+                    </div>
+                    <div className={style.matchContainer}>
+                        {leagueMatches?.map((m) => {
+                            <div className={style.card}>
+                                <p>{m.matchId}</p>
+                            </div>
+                        })}
+                    </div>
                 </div>
             </div>
         </>
