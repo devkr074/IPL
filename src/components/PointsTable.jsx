759ca1dd20bab56f3ddd8b946906c1176290c124
Dev Kumar <devkr074@gmail.com>
2025-05-01 08:46:11 +0530

    Updated Points Table Component

--- src/components/PointsTable.jsx
+++ src/components/PointsTable.jsx
@@ -12,32 +12,32 @@ function PointsTable() {
     }, []);
     return (
         <>
-            <div>
-                <p>IPL - Points Table</p>
+            <div className="row sticky-top shadow">
+                <p className="col-12 fs-5 fw-bold text-light text-center bg-green p-2 m-0">IPL - Points Table</p>
             </div>
-            <div>
+            <div className='row'>
                 <table>
-                    <thead>
-                        <tr>
-                            <th>Team</th>
+                    <thead className='bg-gray'>
+                        <tr className='bg-gray'>
+                            <th className='bg-gray py-2 px-2'>Team</th>
                             <th>P</th>
                             <th>W</th>
                             <th>L</th>
                             <th>T</th>
-                            <th>PTS</th>
-                            <th>NRR</th>
+                            <th className='col-1 px-1 text-center'>PTS</th>
+                            <th className='col-1 px-1 text-center'>NRR</th>
                         </tr>
                     </thead>
                     <tbody>
                         {pointsTable && pointsTable.map((t) => (
                             <tr key={t.teamId}>
-                                <td><img src={teams[t.teamId - 1].logo} alt={teams[t.teamId - 1].name} />{teams[t.teamId - 1].shortName}</td>
-                                <td>{t.played}</td>
-                                <td>{t.won}</td>
-                                <td>{t.lost}</td>
-                                <td>{t.tied}</td>
-                                <th>{t.points}</th>
-                                <td>{(t.netRunRate > 0) ? ("+" + t.netRunRate.toFixed(3)) : (t.netRunRate.toFixed(3))}</td>
+                                <td className='col-6 px-1 py-2'><img className='col-2 me-1' src={teams[t.teamId - 1].logo} alt={teams[t.teamId - 1].name} />{teams[t.teamId - 1].shortName}</td>
+                                <td className='col-1 px-1 py-2'>{t.played}</td>
+                                <td className='col-1'>{t.won}</td>
+                                <td className='col-1'>{t.lost}</td>
+                                <td className='col-1'>{t.tied}</td>
+                                <th className='col-1 px-1 text-center'>{t.points}</th>
+                                <td className='col-1 px-1 text-center'>{(t.netRunRate > 0) ? ("+" + t.netRunRate.toFixed(3)) : (t.netRunRate.toFixed(3))}</td>
                             </tr>
                         ))}
                     </tbody>
