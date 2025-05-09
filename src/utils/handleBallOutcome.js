2c0bfe7da4a2647e3d62cbfd67adb0770af264ca
Dev Kumar <devkr074@gmail.com>
2025-05-09 23:19:45 +0530

    Updated Utils File

--- src/utils/handleBallOutcome.js
+++ src/utils/handleBallOutcome.js
@@ -1,106 +1,169 @@
-function handleBallOutcome(balls, inningBalls, wickets, roleId) {
+function handleBallOutcome(balls, inningBalls, roleId) {
     const outcomes = [];
-    if ((roleId == 1 || roleId == 2) && balls <= 10 && inningBalls <= 96) {
-        outcomes.push(...Array(8).fill(0));
-        outcomes.push(...Array(5).fill(1));
-        outcomes.push(...Array(1).fill(2));
-        outcomes.push(...Array(0).fill(3));
-        outcomes.push(...Array(2).fill(4));
-        outcomes.push(...Array(1).fill(5));
-        outcomes.push(...Array(1).fill(6));
-        outcomes.push(...Array(0).fill(7));
-        outcomes.push(...Array(0).fill(8));
-        outcomes.push(...Array(0).fill(9));
-        outcomes.push(...Array(0).fill(10));
-        outcomes.push(...Array(1).fill(11));
-        outcomes.push(...Array(1).fill(12));
-        outcomes.push(...Array(1).fill(13));
-        outcomes.push(...Array(1).fill(14));
+    if (roleId == 1) {
+        if (inningBalls <= 96) {
+            if (balls <= 10) {
+                outcomes.push(...Array(30).fill(0));
+                outcomes.push(...Array(30).fill(1));
+                outcomes.push(...Array(10).fill(2));
+                outcomes.push(...Array(3).fill(3));
+                outcomes.push(...Array(8).fill(4));
+                outcomes.push(...Array(1).fill(5));
+                outcomes.push(...Array(4).fill(6));
+                outcomes.push(...Array(1).fill(7));
+                outcomes.push(...Array(1).fill(8));
+                outcomes.push(...Array(1).fill(9));
+                outcomes.push(...Array(1).fill(10));
+                outcomes.push(...Array(2).fill(11));
+                outcomes.push(...Array(1).fill(12));
+                outcomes.push(...Array(1).fill(13));
+                outcomes.push(...Array(1).fill(14));
+            }
+            else {
+                outcomes.push(...Array(25).fill(0));
+                outcomes.push(...Array(35).fill(1));
+                outcomes.push(...Array(10).fill(2));
+                outcomes.push(...Array(3).fill(3));
+                outcomes.push(...Array(12).fill(4));
+                outcomes.push(...Array(1).fill(5));
+                outcomes.push(...Array(6).fill(6));
+                outcomes.push(...Array(1).fill(7));
+                outcomes.push(...Array(1).fill(8));
+                outcomes.push(...Array(1).fill(9));
+                outcomes.push(...Array(1).fill(10));
+                outcomes.push(...Array(1).fill(11));
+                outcomes.push(...Array(1).fill(12));
+                outcomes.push(...Array(1).fill(13));
+                outcomes.push(...Array(1).fill(14));
+            }
+        }
+        else {
+            outcomes.push(...Array(27).fill(0));
+            outcomes.push(...Array(36).fill(1));
+            outcomes.push(...Array(7).fill(2));
+            outcomes.push(...Array(2).fill(3));
+            outcomes.push(...Array(11).fill(4));
+            outcomes.push(...Array(1).fill(5));
+            outcomes.push(...Array(7).fill(6));
+            outcomes.push(...Array(1).fill(7));
+            outcomes.push(...Array(1).fill(8));
+            outcomes.push(...Array(1).fill(9));
+            outcomes.push(...Array(1).fill(10));
+            outcomes.push(...Array(2).fill(11));
+            outcomes.push(...Array(1).fill(12));
+            outcomes.push(...Array(1).fill(13));
+            outcomes.push(...Array(1).fill(14));
+        }
     }
-    else if ((roleId == 1 || roleId == 2) && balls > 10 && inningBalls <= 96) {
-        outcomes.push(...Array(8).fill(0));
-        outcomes.push(...Array(8).fill(1));
-        outcomes.push(...Array(2).fill(2));
-        outcomes.push(...Array(1).fill(3));
-        outcomes.push(...Array(4).fill(4));
-        outcomes.push(...Array(1).fill(5));
-        outcomes.push(...Array(2).fill(6));
-        outcomes.push(...Array(0).fill(7));
-        outcomes.push(...Array(0).fill(8));
-        outcomes.push(...Array(0).fill(9));
-        outcomes.push(...Array(0).fill(10));
-        outcomes.push(...Array(1).fill(11));
-        outcomes.push(...Array(1).fill(12));
-        outcomes.push(...Array(1).fill(13));
-        outcomes.push(...Array(1).fill(14));
+    else if (roleId == 2) {
+        if (inningBalls <= 96) {
+            if (balls <= 10) {
+                outcomes.push(...Array(28).fill(0));
+                outcomes.push(...Array(27).fill(1));
+                outcomes.push(...Array(9).fill(2));
+                outcomes.push(...Array(3).fill(3));
+                outcomes.push(...Array(8).fill(4));
+                outcomes.push(...Array(1).fill(5));
+                outcomes.push(...Array(5).fill(6));
+                outcomes.push(...Array(1).fill(7));
+                outcomes.push(...Array(1).fill(8));
+                outcomes.push(...Array(1).fill(9));
+                outcomes.push(...Array(1).fill(10));
+                outcomes.push(...Array(2).fill(11));
+                outcomes.push(...Array(1).fill(12));
+                outcomes.push(...Array(1).fill(13));
+                outcomes.push(...Array(1).fill(14));
+            }
+            else {
+                outcomes.push(...Array(23).fill(0));
+                outcomes.push(...Array(30).fill(1));
+                outcomes.push(...Array(10).fill(2));
+                outcomes.push(...Array(4).fill(3));
+                outcomes.push(...Array(11).fill(4));
+                outcomes.push(...Array(1).fill(5));
+                outcomes.push(...Array(7).fill(6));
+                outcomes.push(...Array(1).fill(7));
+                outcomes.push(...Array(1).fill(8));
+                outcomes.push(...Array(1).fill(9));
+                outcomes.push(...Array(1).fill(10));
+                outcomes.push(...Array(2).fill(11));
+                outcomes.push(...Array(1).fill(12));
+                outcomes.push(...Array(1).fill(13));
+                outcomes.push(...Array(1).fill(14));
+            }
+        }
+        else {
+            outcomes.push(...Array(23).fill(0));
+            outcomes.push(...Array(30).fill(1));
+            outcomes.push(...Array(10).fill(2));
+            outcomes.push(...Array(4).fill(3));
+            outcomes.push(...Array(11).fill(4));
+            outcomes.push(...Array(1).fill(5));
+            outcomes.push(...Array(7).fill(6));
+            outcomes.push(...Array(1).fill(7));
+            outcomes.push(...Array(1).fill(8));
+            outcomes.push(...Array(1).fill(9));
+            outcomes.push(...Array(1).fill(10));
+            outcomes.push(...Array(2).fill(11));
+            outcomes.push(...Array(1).fill(12));
+            outcomes.push(...Array(1).fill(13));
+            outcomes.push(...Array(1).fill(14));
+        }
     }
-    else if ((roleId == 1 || roleId == 2) && wickets <= 5 && inningBalls > 96) {
-        outcomes.push(...Array(8).fill(0));
-        outcomes.push(...Array(10).fill(1));
-        outcomes.push(...Array(3).fill(2));
-        outcomes.push(...Array(1).fill(3));
-        outcomes.push(...Array(6).fill(4));
-        outcomes.push(...Array(1).fill(5));
-        outcomes.push(...Array(4).fill(6));
-        outcomes.push(...Array(0).fill(7));
-        outcomes.push(...Array(0).fill(8));
-        outcomes.push(...Array(0).fill(9));
-        outcomes.push(...Array(0).fill(10));
-        outcomes.push(...Array(1).fill(11));
-        outcomes.push(...Array(1).fill(12));
-        outcomes.push(...Array(1).fill(13));
-        outcomes.push(...Array(1).fill(14));
-    }
-    else if ((roleId == 1 || roleId == 2) && wickets > 5 && inningBalls > 96) {
-        outcomes.push(...Array(8).fill(0));
-        outcomes.push(...Array(5).fill(1));
-        outcomes.push(...Array(1).fill(2));
-        outcomes.push(...Array(0).fill(3));
-        outcomes.push(...Array(2).fill(4));
-        outcomes.push(...Array(1).fill(5));
-        outcomes.push(...Array(1).fill(6));
-        outcomes.push(...Array(1).fill(7));
-        outcomes.push(...Array(0).fill(8));
-        outcomes.push(...Array(0).fill(9));
-        outcomes.push(...Array(0).fill(10));
-        outcomes.push(...Array(1).fill(11));
-        outcomes.push(...Array(1).fill(12));
-        outcomes.push(...Array(1).fill(13));
-        outcomes.push(...Array(1).fill(14));
-    }
-    else if (roleId == 3 && inningBalls <= 96) {
-        outcomes.push(...Array(8).fill(0));
-        outcomes.push(...Array(4).fill(1));
-        outcomes.push(...Array(1).fill(2));
-        outcomes.push(...Array(0).fill(3));
-        outcomes.push(...Array(1).fill(4));
-        outcomes.push(...Array(1).fill(5));
-        outcomes.push(...Array(0).fill(6));
-        outcomes.push(...Array(1).fill(7));
-        outcomes.push(...Array(1).fill(8));
-        outcomes.push(...Array(0).fill(9));
-        outcomes.push(...Array(0).fill(10));
-        outcomes.push(...Array(1).fill(11));
-        outcomes.push(...Array(1).fill(12));
-        outcomes.push(...Array(1).fill(13));
-        outcomes.push(...Array(1).fill(14));
-    }
-    else if (roleId == 3 && inningBalls > 96) {
-        outcomes.push(...Array(8).fill(0));
-        outcomes.push(...Array(5).fill(1));
-        outcomes.push(...Array(1).fill(2));
-        outcomes.push(...Array(0).fill(3));
-        outcomes.push(...Array(1).fill(4));
-        outcomes.push(...Array(1).fill(5));
-        outcomes.push(...Array(1).fill(6));
-        outcomes.push(...Array(1).fill(7));
-        outcomes.push(...Array(0).fill(8));
-        outcomes.push(...Array(1).fill(9));
-        outcomes.push(...Array(1).fill(10));
-        outcomes.push(...Array(1).fill(11));
-        outcomes.push(...Array(1).fill(12));
-        outcomes.push(...Array(1).fill(13));
-        outcomes.push(...Array(1).fill(14));
+    else if (roleId == 3) {
+        if (inningBalls <= 96) {
+            if (balls <= 10) {
+                outcomes.push(...Array(35).fill(0));
+                outcomes.push(...Array(12).fill(1));
+                outcomes.push(...Array(4).fill(2));
+                outcomes.push(...Array(2).fill(3));
+                outcomes.push(...Array(1).fill(4));
+                outcomes.push(...Array(1).fill(5));
+                outcomes.push(...Array(1).fill(6));
+                outcomes.push(...Array(1).fill(7));
+                outcomes.push(...Array(1).fill(8));
+                outcomes.push(...Array(1).fill(9));
+                outcomes.push(...Array(1).fill(10));
+                outcomes.push(...Array(2).fill(11));
+                outcomes.push(...Array(1).fill(12));
+                outcomes.push(...Array(1).fill(13));
+                outcomes.push(...Array(1).fill(14));
+            }
+            else {
+                outcomes.push(...Array(26).fill(0));
+                outcomes.push(...Array(10).fill(1));
+                outcomes.push(...Array(4).fill(2));
+                outcomes.push(...Array(2).fill(3));
+                outcomes.push(...Array(2).fill(4));
+                outcomes.push(...Array(1).fill(5));
+                outcomes.push(...Array(1).fill(6));
+                outcomes.push(...Array(1).fill(7));
+                outcomes.push(...Array(1).fill(8));
+                outcomes.push(...Array(1).fill(9));
+                outcomes.push(...Array(1).fill(10));
+                outcomes.push(...Array(2).fill(11));
+                outcomes.push(...Array(1).fill(12));
+                outcomes.push(...Array(1).fill(13));
+                outcomes.push(...Array(1).fill(14));
+            }
+        }
+        else {
+            outcomes.push(...Array(26).fill(0));
+            outcomes.push(...Array(10).fill(1));
+            outcomes.push(...Array(4).fill(2));
+            outcomes.push(...Array(2).fill(3));
+            outcomes.push(...Array(2).fill(4));
+            outcomes.push(...Array(1).fill(5));
+            outcomes.push(...Array(1).fill(6));
+            outcomes.push(...Array(1).fill(7));
+            outcomes.push(...Array(1).fill(8));
+            outcomes.push(...Array(1).fill(9));
+            outcomes.push(...Array(1).fill(10));
+            outcomes.push(...Array(2).fill(11));
+            outcomes.push(...Array(1).fill(12));
+            outcomes.push(...Array(1).fill(13));
+            outcomes.push(...Array(1).fill(14));
+        }
     }
     for (let i = outcomes.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
